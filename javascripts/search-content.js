e1b064f13e0990b9f21dd1932b79ec3b5fdb53b4
Mamatha Munivelu <munivelu.mamatha@accenture.com>
2012-11-21 15:26:45 +0530

    yes

--- javascripts/search-content.js
+++ javascripts/search-content.js
@@ -670,7 +670,7 @@ function search() {
 				
 					typeImage ='<span class="jive-icon-med jive-icon-document"></span>';
 					mainId="all_DOC-"+allId;
-				}else (row.type=="post"){
+				}else if(row.type=="post"){
 					var postDetailsId=row.resources.self.ref;
 					var blogSummaryId=row.blogSummary.resources.self.ref;
 					var blogId = (blogSummaryId.substring(blogSummaryId.lastIndexOf("/"))).substr(1);
